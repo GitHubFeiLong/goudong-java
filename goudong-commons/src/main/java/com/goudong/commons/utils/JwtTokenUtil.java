408b8be6b921d874755739529e689fea418519bb
CFL <169madman@gmail.com>
2021-05-24 18:00:09 +0800

    异常有问题

--- goudong-commons/src/main/java/com/goudong/commons/utils/JwtTokenUtil.java
+++ goudong-commons/src/main/java/com/goudong/commons/utils/JwtTokenUtil.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.IdUtil;
 import com.alibaba.fastjson.JSON;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
@@ -109,9 +110,10 @@ public class JwtTokenUtil {
                 .build();
 
         DecodedJWT jwt = null;
+        BasicException.exception(ClientExceptionEnum.UNAUTHORIZED);
         try {
             jwt = verifier.verify(nativeToken);
-        } catch (TokenExpiredException e) {
+        } catch (TokenExpiredException | JWTDecodeException e) {
             BasicException.exception(ClientExceptionEnum.UNAUTHORIZED);
         }
 
--- goudong-message-server/src/main/java/com/goudong/message/controller/CodeController.java
+++ goudong-message-server/src/main/java/com/goudong/message/controller/CodeController.java
@@ -1,15 +1,16 @@
 package com.goudong.message.controller;
 
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.goudong.commons.enumerate.RedisKeyEnum;
 import com.goudong.commons.pojo.Result;
 import com.goudong.commons.utils.AssertUtil;
 import com.goudong.commons.utils.RedisValueUtil;
 import com.goudong.message.config.CodeDirectRabbitConfig;
 import com.goudong.message.sender.EmailCodeRoutingKeySender;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,7 @@ import javax.validation.constraints.Email;
  * @Date 2021-05-05 10:59
  * @Version 1.0
  */
+@Api(tags="验证码")
 @Validated
 @RestController
 @RequestMapping("/code")
@@ -48,6 +50,8 @@ public class CodeController {
      * @return
      */
     @GetMapping("/email-code/{email}")
+    @ApiOperation(value = "发送邮箱验证码")
+    @ApiImplicitParam(name = "email", value = "邮箱", required = true)
     public Result sendEmailCode (@PathVariable("email") @Email(message = "请输入正确邮箱格式") String email) {
         rabbitTemplate.convertAndSend(CodeDirectRabbitConfig.CODE_DIRECT_EXCHANGE, CodeDirectRabbitConfig.EMAIL_CODE_ROUTING_KEY, email);
         return Result.ofSuccess();
@@ -58,6 +62,8 @@ public class CodeController {
      * @return
      */
     @GetMapping("/phone-code/{phone}")
+    @ApiOperation(value = "发送手机验证码")
+    @ApiImplicitParam(name = "phone", value = "手机号码", required = true)
     public Result sendPhoneCode (@PathVariable("phone") String phone) {
         AssertUtil.isPhone(phone, "手机号格式错误：" + phone);
         rabbitTemplate.convertAndSend(CodeDirectRabbitConfig.CODE_DIRECT_EXCHANGE, CodeDirectRabbitConfig.PHONE_CODE_ROUTING_KEY, phone);
@@ -71,6 +77,11 @@ public class CodeController {
      * @return
      */
     @GetMapping("/check-code/{number}/{code}")
+    @ApiOperation(value = "验证验证码是否正确")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "number", value = "手机号码/邮箱", required = true),
+        @ApiImplicitParam(name = "code", value = "验证码", required = true)
+    })
     public Result<Boolean> checkCode (@PathVariable String number, @PathVariable String code) {
         String redisValue = redisValueUtil.getValue(RedisKeyEnum.MESSAGE_AUTH_CODE, number);
         if (redisValue == null || !code.equals(redisValue)) {
