dcdcbbbbddf72da86aecd79d282bed0fa2b9cf66
CFL <169madman@gmail.com>
2021-05-25 16:09:37 +0800

    OAA 服务，

--- goudong-oaa-server/src/main/java/com/goudong/oaa/OaaApplication.java
+++ goudong-oaa-server/src/main/java/com/goudong/oaa/OaaApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * http://localhost:10003/oauth/authorize?response_type=code&client_id=admin&redirect_uri=http://www.baidu.com&scope=all&state=normal
+ * 教程：https://juejin.cn/post/6844903987137740813
+ * 访问上面的路径获取code
  * @Author msi
  * @Date 2021-05-25 14:06
  * @Version 1.0
