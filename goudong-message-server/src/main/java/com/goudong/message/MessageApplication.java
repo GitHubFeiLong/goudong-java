1b17e7e3270cafbeb1a4d0a4113b971c31581a0f
MSI <169madman@gmail.com>
2021-08-16 21:13:32 +0800

    https://blog.csdn.net/weixin_28873283/article/details/112833287

--- goudong-message-server/src/main/java/com/goudong/message/MessageApplication.java
+++ goudong-message-server/src/main/java/com/goudong/message/MessageApplication.java
@@ -1,11 +1,9 @@
 package com.goudong.message;
 
-import com.goudong.commons.config.WebMvcConfiguration;
 import com.goudong.commons.constant.BasePackageConst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
