eaab940a5741a4e3329897c092c9a7dc4e16fc76
BitbucketFeiLong <1696741038@qq.com>
2021-11-30 08:39:22 +0800

    初始化  file online preview

---
+++ goudong-file-online-preview/src/main/java/com/goudong/file/online/preview/FileOnlinePreviewApplication.java
@@ -0,0 +1,16 @@
+package com.goudong.file.online.preview;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类描述：
+ *
+ * @Author e-Feilong.Chen
+ * @Date 2021/11/30 8:26
+ */
+@SpringBootApplication
+public class FileOnlinePreviewApplication {
+    public static void main(String[] args) {
+
+    }
+}
