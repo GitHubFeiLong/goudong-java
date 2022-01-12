f65f621236dc6a92170e825c3de8639ff19a088e
BitbucketFeiLong <1696741038@qq.com>
2022-01-12 17:08:28 +0800

    redis

---
+++ goudong-commons/src/test/java/com/goudong/commons/utils/core/AssertUtilTest.java
@@ -0,0 +1,42 @@
+package com.goudong.commons.utils.core;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssertUtilTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void isPhone() {
+    }
+
+    @Test
+    void isEmail() {
+    }
+
+    @Test
+    void isEnum() {
+    }
+
+    @Test
+    void testIsEnum() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void isDiskPath() {
+    }
+}
