74cba8fc9e4f6384b4e327bb0838ae4c69220034
msi <1696741038@qq.com>
2022-02-04 14:50:25 +0800

    认证流程图，认证日志记录完成

--- goudong-oauth2-server/src/main/resources/db/migration/V0.0.4__create_base_authentication_log.sql
+++ goudong-oauth2-server/src/main/resources/db/migration/V0.0.4__create_base_authentication_log.sql
@@ -5,6 +5,7 @@ CREATE TABLE `base_authentication_log` (
    `ip` int(10) unsigned DEFAULT NULL COMMENT 'ip地址,使用整数存储，当获取不到ip时使用0',
    `successful` bit(1) NOT NULL DEFAULT b'1' COMMENT '是否是认证成功',
    `type` varchar(16) NOT NULL DEFAULT 'system' COMMENT '认证类型（system:系统用户,qq:qq快捷登录，wei_xin：微信快捷登录）',
+   `description` varchar(255) DEFAULT NULL COMMENT '描述，失败时记录失败原因',
    `deleted` bit(1) DEFAULT b'0' COMMENT '是否被删除',
    `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
