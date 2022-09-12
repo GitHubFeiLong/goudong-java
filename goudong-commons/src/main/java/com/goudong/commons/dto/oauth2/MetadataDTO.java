75b9ca4a7f84d318f8a2ec779e2757e8455c3015
陈飞龙 <1696741038@qq.com>
2022-09-12 21:27:57 +0800

    输出格式修改

--- goudong-commons/src/main/java/com/goudong/commons/dto/oauth2/MetadataDTO.java
+++ goudong-commons/src/main/java/com/goudong/commons/dto/oauth2/MetadataDTO.java
@@ -15,13 +15,23 @@ public class MetadataDTO {
     //~fields
     //==================================================================================================================
     /**
-     * 请求路径，
+     *
      */
-    private String url;
+    private String name;
+
+    /**
+     * 是否是api
+     */
+    private Boolean api = false;
+
+    /**
+     * 前端的路由或后端的接口，
+     */
+    private String path;
 
     /**
      * 请求方式,值为{@link HttpMethod}元素的name(),例如：GET，POST。
-     * 这里url 和 method 是一对一的方式，方便更细粒度鉴权。
+     * 这里path 和 method 是一对一的方式，方便更细粒度鉴权。
      */
     private String method;
 
