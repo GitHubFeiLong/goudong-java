0b7e561635b38864550e8007122c031c419c25cd
1696741038@qq.com <1696741038@qq.com>
2022-10-14 17:43:22 +0800

    rabbitmq

--- goudong-modules/rabbitmq-demo/src/main/java/com/goudong/modules/rabbitmq/demo/dead/letter/DeadLetterRecv.java
+++ goudong-modules/rabbitmq-demo/src/main/java/com/goudong/modules/rabbitmq/demo/dead/letter/DeadLetterRecv.java
@@ -1,9 +1,40 @@
-package com.goudong.modules.rabbitmq.demo.dead.letter;/**
+package com.goudong.modules.rabbitmq.demo.dead.letter;
+
+import com.goudong.modules.rabbitmq.demo.util.ConnectionUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
  * 类描述：
- * 
+ * 死信队列的消费者
  * @author cfl
- * @date 2022/10/14 16:17
  * @version 1.0
+ * @date 2022/10/14 16:17
  */
 public class DeadLetterRecv {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 获取连接
+        Connection connection = ConnectionUtil.getConnection();
+        // 创建通道
+        Channel channel = connection.createChannel();
+        // 申明死信交换机
+        channel.exchangeDeclare("test.dead.letter.exchange", "direct", true);
+        // 申明死信队列（队列不存在时，会出现异常）
+        channel.queueDeclare("test.dead.letter.queue", true, false, false, null);
+        // 绑定队列交换机路由key
+        channel.queueBind("test.dead.letter.queue", "test.dead.letter.exchange", "test.dead.letter.routing.key", null);
+
+        // 接收消息
+        channel.basicConsume("test.dead.letter.queue", false, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("死信队列接收到消息：" + new String(body));
+
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        });
+    }
 }
--- goudong-modules/rabbitmq-demo/src/main/java/com/goudong/modules/rabbitmq/demo/dead/letter/Send1.java
+++ goudong-modules/rabbitmq-demo/src/main/java/com/goudong/modules/rabbitmq/demo/dead/letter/Send1.java
@@ -1,9 +1,44 @@
-package com.goudong.modules.rabbitmq.demo.dead.letter;/**
+package com.goudong.modules.rabbitmq.demo.dead.letter;
+
+import com.goudong.modules.rabbitmq.demo.util.ConnectionUtil;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+/**
  * 类描述：
- * 
+ *
  * @author cfl
- * @date 2022/10/14 16:06
  * @version 1.0
+ * @date 2022/10/14 16:06
  */
 public class Send1 {
+    public static final String QUEUE_NAME = "test.queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+
+        Channel channel = connection.createChannel();
+
+        boolean durable = true;
+        Map<String, Object> arguments = new HashMap<>();
+        // 这里声明当前队列绑定的死信交换机
+        arguments.put("x-dead-letter-exchange", "test.dead.letter.exchange");
+        // 这里声明当前队列的死信路由key
+        arguments.put("x-dead-letter-routing-key", "test.dead.letter.routing.key");
+        // 这里设置队列中所有消息的有效期是5s
+        arguments.put("x-message-ttl", 5000);
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, arguments);
+
+        channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, "send message".getBytes("utf-8"));
+
+        channel.close();
+        connection.close();
+    }
 }
