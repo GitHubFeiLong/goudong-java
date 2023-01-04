47a1e86c73c47d6aedc036ea48f6e13e02582e4e
1696741038@qq.com <1696741038@qq.com>
2023-01-04 17:08:09 +0800

    gateway 即将维护

--- goudong-gateway-server/src/main/java/com/goudong/gateway/filter/ReqResBodyCryptoFilter.java
+++ goudong-gateway-server/src/main/java/com/goudong/gateway/filter/ReqResBodyCryptoFilter.java
@@ -1,7 +1,6 @@
 package com.goudong.gateway.filter;
 
 import com.goudong.commons.constant.core.HttpHeaderConst;
-import com.goudong.commons.utils.core.LogUtil;
 import com.goudong.core.security.aes.AES;
 import com.goudong.core.security.rsa.ServerRSA;
 import com.goudong.core.util.StringUtil;
@@ -19,18 +18,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * 类描述：
@@ -61,10 +54,23 @@ public class ReqResBodyCryptoFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        // 非post请求直接放行
-        if (request.getMethod() != HttpMethod.POST) {
+        // 查看请求头是否自定义的请求头(X-Aes-Key)
+        String aesKeyEncrypt = request.getHeaders().getFirst(HttpHeaderConst.X_AES_KEY);
+        // 没有加密请求参数，直接放行
+        if(StringUtil.isBlank(aesKeyEncrypt)){
             return chain.filter(exchange);
         }
+
+        HttpMethod method = request.getMethod();
+
+        switch (method) {
+            case POST:
+                return operationExchangeByPost(exchange, chain);
+            default:
+                return chain.filter(exchange);
+        }
+
+
         // if (true) {
         //     // 查看请求头是否自定义的请求头(X-Aes-Key)
         //     String aesKeyEncrypt = exchange.getRequest().getHeaders().getFirst(HttpHeaderConst.X_AES_KEY);
@@ -88,66 +94,61 @@ public class ReqResBodyCryptoFilter implements GlobalFilter, Ordered {
         //
         //
         // }
-        // post请求处理
-        return operationExchange(exchange, chain);
     }
 
     /**
-     * 修改请求参数的具体逻辑
+     * 修改POST请求的body的具体逻辑
      * TODO 这个还没完全理解，Gateway后面要系统的再学一遍
      * @param exchange
      * @param chain
      * @return
      */
-    private Mono<Void> operationExchange(ServerWebExchange exchange, GatewayFilterChain chain) {
+    private Mono<Void> operationExchangeByPost(ServerWebExchange exchange, GatewayFilterChain chain) {
         // 查看请求头是否自定义的请求头(X-Aes-Key)
         String aesKeyEncrypt = exchange.getRequest().getHeaders().getFirst(HttpHeaderConst.X_AES_KEY);
-        if(StringUtil.isNotBlank(aesKeyEncrypt)){
-            // 使用RSA私钥解密密文的AES密钥
-            String aesKey = ServerRSA.getInstance().privateKeyDecrypt(aesKeyEncrypt);
-            LogUtil.info(log,"本次请求使用了AES解密,AES密钥为{}", aesKey);
+        // 使用RSA私钥解密密文的AES密钥
+        String aesKey = ServerRSA.getInstance().privateKeyDecrypt(aesKeyEncrypt);
+        log.debug("本次请求使用了AES解密,AES密钥为{}", aesKey);
 
-            // 修改请求头
-            modifyRequestHeaders(exchange, aesKey);
+        // 修改请求头
+        modifyRequestHeaders(exchange, aesKey);
 
-            // 获取请求的媒体类型(application/json等)
-            MediaType mediaType = exchange.getRequest().getHeaders().getContentType();
-            // 读取请求体并使用AES解密请求参数
-            ServerRequest serverRequest = new DefaultServerRequest(exchange);
-            Mono<String> modifiedBody = serverRequest.bodyToMono(String.class)
-                    .flatMap(body -> {
-                        if (MediaType.APPLICATION_JSON.isCompatibleWith(mediaType)) {
-                            // 对原先的body进行修改操作
-                            // 前端加密后带双引号的字符串
-                            body = body.startsWith("\"") ? body.substring(1, body.length()-1) : body;
-                            return Mono.just(AES.build()
-                                    .secretKey(aesKey)
-                                    .decrypt(body)
-                            );
-                        }
-                        return Mono.empty();
-                    });
-            BodyInserter bodyInserter = BodyInserters.fromPublisher(modifiedBody, String.class);
+        // 获取请求的媒体类型(application/json等)
+        MediaType mediaType = exchange.getRequest().getHeaders().getContentType();
+        // 读取请求体并使用AES解密请求参数
+        ServerRequest serverRequest = new DefaultServerRequest(exchange);
+        Mono<String> modifiedBody = serverRequest.bodyToMono(String.class)
+                .flatMap(body -> {
+                    log.debug("lllllll");
+                    if (MediaType.APPLICATION_JSON.isCompatibleWith(mediaType)) {
+                        // 对原先的body进行修改操作
+                        // 前端加密后带双引号的字符串
+                        body = body.startsWith("\"") ? body.substring(1, body.length()-1) : body;
+                        return Mono.just(AES.build()
+                                .secretKey(aesKey)
+                                .decrypt(body)
+                        );
+                    }
+                    return Mono.empty();
+                });
+        BodyInserter bodyInserter = BodyInserters.fromPublisher(modifiedBody, String.class);
 
-            /*
-                创建新的请求
-             */
-            HttpHeaders headers = new HttpHeaders();
-            headers.putAll(exchange.getRequest().getHeaders());
-            headers.remove(HttpHeaders.CONTENT_LENGTH);
-            CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange, headers);
-            return bodyInserter.insert(outputMessage, new BodyInserterContext())
-                    .then(Mono.defer(() -> {
-                        // 创建自定义请求解码器
-                        RequestDecodeHandler requestDecodeHandler = new RequestDecodeHandler(exchange.getRequest(), outputMessage);
-                        // 创建自定义响应解码器
-                        ResponseDecodeHandler responseDecodeHandler = new ResponseDecodeHandler(exchange.getResponse(), aesKey);
+        /*
+            创建新的请求
+         */
+        HttpHeaders headers = new HttpHeaders();
+        headers.putAll(exchange.getRequest().getHeaders());
+        headers.remove(HttpHeaders.CONTENT_LENGTH);
+        CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange, headers);
+        return bodyInserter.insert(outputMessage, new BodyInserterContext())
+                .then(Mono.defer(() -> {
+                    // 创建自定义请求解码器
+                    RequestDecodeHandler requestDecodeHandler = new RequestDecodeHandler(exchange.getRequest(), outputMessage);
+                    // 创建自定义响应解码器
+                    ResponseDecodeHandler responseDecodeHandler = new ResponseDecodeHandler(exchange.getResponse(), aesKey);
 
-                        return chain.filter(exchange.mutate().request(requestDecodeHandler).response(responseDecodeHandler).build());
-                    }));
-        }
-
-        return chain.filter(exchange);
+                    return chain.filter(exchange.mutate().request(requestDecodeHandler).response(responseDecodeHandler).build());
+                }));
     }
 
     /**
