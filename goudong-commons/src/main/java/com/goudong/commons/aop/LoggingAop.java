fe8f9f403a76cb9ed3d1391bfdf65ea4c8593090
cfl <1696741038@qq.com>
2023-04-13 11:46:36 +0800

    服务层接口日志打印

--- goudong-commons/src/main/java/com/goudong/commons/aop/LoggingAop.java
+++ goudong-commons/src/main/java/com/goudong/commons/aop/LoggingAop.java
@@ -1,7 +1,13 @@
 package com.goudong.commons.aop;
 
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.goudong.commons.constant.core.SpringProfileConst;
+import com.goudong.core.util.ListUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.RequestFacade;
+import org.apache.catalina.connector.ResponseFacade;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
@@ -13,7 +19,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 类描述：
@@ -27,6 +37,17 @@ import java.util.Arrays;
 @Aspect
 public class LoggingAop {
 
+    private static ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        // transient 忽略属性
+        objectMapper.configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true);
+        // 修改时间格式
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    }
+
     private final Environment env;
 
     public LoggingAop(Environment env) {
@@ -37,26 +58,21 @@ public class LoggingAop {
     }
 
     /**
-     * @RestController，@Controller,@Service 注解的切点
+     * 注解的切点
      * mapper注解不生效，直接切mapper所在的包
      */
     @Pointcut(
-        "@within(org.springframework.stereotype.Service)" +
-        " || @within(org.springframework.web.bind.annotation.RestController)" +
-        " || @within(org.springframework.stereotype.Controller)" +
-        " || within(com.goudong.*.mapper..*)"
+        "@within(org.springframework.stereotype.Service)"
     )
     public void springBeanPointcut() {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
     }
 
     /**
-     * 应用的控制层，业务层，持久层的包切点
+     * 业务层，持久层的包切点
      */
     @Pointcut(
-        "within(com.goudong.*.controller..*)" +
-        " || within(com.goudong.*.service..*)" +
-        " || within(com.goudong.*.mapper..*)"
+        "within(com.goudong.*.service..*)"
     )
     public void applicationPackagePointcut() {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
@@ -109,17 +125,59 @@ public class LoggingAop {
      * @return result.
      * @throws Throwable throws {@link IllegalArgumentException}.
      */
-    @Around("springBeanPointcut() && springBeanPointcut()")
+    @Around("springBeanPointcut() && applicationPackagePointcut()")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         Logger log = logger(joinPoint);
+
+        // 过滤掉部分参数
+        List<Object> args = getArgs(joinPoint);
+        String params;
+        try {
+            params = objectMapper.writeValueAsString(args);
+        } catch (Exception e) {
+            log.warn("对象序列化json失败：{}", e);
+            params = args.toString();
+        }
+
         if (log.isDebugEnabled()) {
-            log.debug("Enter: {}() with argument[s] = {}", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+            log.debug("Enter: {}() with argument[s] = {}", joinPoint.getSignature().getName(), params);
         }
         Object result = joinPoint.proceed();
+        String resultStr;
+        try {
+            resultStr = objectMapper.writeValueAsString(result);
+        } catch (Exception e) {
+            log.warn("对象序列化json失败：{}", e);
+            resultStr = args.toString();
+        }
+
         if (log.isDebugEnabled()) {
-            log.debug("Exit: {}() with result = {}", joinPoint.getSignature().getName(), result);
+            log.debug("Exit: {}() with result = {}", joinPoint.getSignature().getName(), resultStr);
         }
         return result;
     }
 
+    /**
+     * 获取请求参数
+     * @param joinPoint
+     * @return
+     */
+    private List<Object> getArgs(ProceedingJoinPoint joinPoint) {
+        List<Class> filter = ListUtil.newArrayList(
+                RequestFacade.class,
+                ResponseFacade.class
+        );
+        Object[] argsArr = joinPoint.getArgs();
+        // Stream.of(null).collect(Collectors.toList()) 会出现NPE
+        if (argsArr != null && argsArr.length > 0) {
+            log.debug("argsArr ＝{}", argsArr);
+            // 过滤掉大对象，避免转json报错
+            return Stream.of(argsArr)
+                    // 过滤掉
+                    .filter(f -> f != null && !filter.contains(f.getClass()))
+                    .collect(Collectors.toList());
+        }
+
+        return new ArrayList<>(0);
+    }
 }
