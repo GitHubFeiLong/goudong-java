63bb23baeda702e244be1f84c889af51bc0e5d8b
cfl <1696741038@qq.com>
2023-05-12 10:09:20 +0800

    nio demo

--- goudong-modules/netty/src/main/java/com/goudong/modules/netty/nio/example1/Client.java
+++ goudong-modules/netty/src/main/java/com/goudong/modules/netty/nio/example1/Client.java
@@ -3,11 +3,11 @@ package com.goudong.modules.netty.nio.example1;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Set;
 
 /**
@@ -24,51 +24,100 @@ public class Client {
     //~methods
     //==================================================================================================================
     public static void main(String[] args) throws IOException {
-        Selector selector = Selector.open();
-        SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.configureBlocking(false);
+        // 创建选择器
+        Selector selector1 = Selector.open();
+        // 创建通道
+        SocketChannel socketChannel1 = SocketChannel.open();
 
-        /*非阻塞的连接*/
-        if(socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080))){
-            socketChannel.register(selector,SelectionKey.OP_READ);
-        }else{
-            socketChannel.register(selector,SelectionKey.OP_CONNECT);
-        }
+        // 创建线程，用以建立连接处理收发数据
+        new Thread(){
+            private Selector selector = selector1;
+            private SocketChannel socketChannel = socketChannel1;
+            @Override
+            public void run() {
+                try {
+                    // 设置非阻塞
+                    socketChannel.configureBlocking(false);
+                    /*非阻塞的连接*/
+                    // 连接服务端（返回true，表示连接成功，注册读事件；返回false，表示还未连接成功，注册连接事件）
+                    if(socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080))){
+                        socketChannel.register(selector,SelectionKey.OP_READ);
+                    }else{
+                        socketChannel.register(selector,SelectionKey.OP_CONNECT);
+                    }
 
-        while (true) {
-            selector.select();
-            Set<SelectionKey> selectionKeys = selector.selectedKeys();
-            Iterator<SelectionKey> iterator = selectionKeys.iterator();
-            while (iterator.hasNext()) {
-                SelectionKey selectionKey = iterator.next();
-                iterator.remove();
 
-                if (selectionKey.isValid()) {
-                    if (selectionKey.isReadable()) {
-                        SocketChannel channel = (SocketChannel)selectionKey.channel();
-                        if(selectionKey.isConnectable()){
-                            if(channel.finishConnect()){
-                                socketChannel.register(selector,
-                                        SelectionKey.OP_READ);}
-                            else System.exit(1);
+                    while (true) {
+                        // 获取选择器中就绪的事件
+                        selector.select();
+                        // 获取就绪事件
+                        Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                        // 迭代器循环
+                        Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                        while (iterator.hasNext()) {
+                            // 获取事件
+                            SelectionKey selectionKey = iterator.next();
+                            // 我们必须首先将处理过的 SelectionKey 从选定的键集合中删除。如果我们没有删除处理过的键，那么它仍然会在主集合中以一个激活的键出现，这会导致我们尝试再次处理它。
+                            iterator.remove();
 
-                            if (selectionKey.isReadable()) {
-                                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                                int read = channel.read(byteBuffer);
-                                if (read > 0) {
-                                    byteBuffer.flip();
-                                    //根据缓冲区可读字节数创建字节数组
-                                    byte[] bytes = new byte[byteBuffer.remaining()];
-                                    //将缓冲区可读字节数组复制到新建的数组中
-                                    byteBuffer.get(bytes);
-                                    String result = new String(bytes,"UTF-8");
-                                    System.out.println("客户端收到消息：" + result);
+                            // 判断键是否有效，一个键在创建时是有效的，并且一直保持有效，直到它被取消、它的通道被关闭或它的选择器被关闭。
+                            if (selectionKey.isValid()) {
+                                // 连接事件
+                                if(selectionKey.isConnectable()){
+                                    // 完成连接后，注册读事件
+                                    if(socketChannel.finishConnect()){
+                                        socketChannel.register(selector, SelectionKey.OP_READ);}
+                                    else {
+                                        System.exit(1);
+                                    }
+                                }
+                                // 处理读事件
+                                if (selectionKey.isReadable()) {
+                                    // 获取注册读事件的客户端SocketChannel
+                                    SocketChannel channel = (SocketChannel)selectionKey.channel();
+                                    // 创建一个缓冲区
+                                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                                    // 从通道中读取数据，保存到缓冲区中
+                                    int read = channel.read(byteBuffer);
+                                    // 读取到数据
+                                    if (read > 0) {
+                                        // 切换读模式
+                                        byteBuffer.flip();
+                                        // 根据缓冲区可读字节数创建字节数组
+                                        byte[] bytes = new byte[byteBuffer.remaining()];
+                                        // 将缓冲区可读字节数组复制到新建的数组中
+                                        byteBuffer.get(bytes);
+                                        String result = new String(bytes,"UTF-8");
+                                        System.out.println("客户端收到消息：" + result);
+                                    } else if (read < 0) {
+                                        // 取消键
+                                        selectionKey.cancel();
+                                        // 关闭通道
+                                        channel.close();
+                                    }
                                 }
                             }
                         }
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+        }.start();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            //将消息编码为字节数组
+            byte[] bytes = scanner.nextLine().getBytes();
+            //根据数组容量创建ByteBuffer
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            //将字节数组复制到缓冲区
+            writeBuffer.put(bytes);
+            //flip操作
+            writeBuffer.flip();
+            //发送缓冲区的字节数组
+            /*关心事件和读写网络并不冲突*/
+            socketChannel1.write(writeBuffer);
         }
     }
 }
--- goudong-modules/netty/src/main/java/com/goudong/modules/netty/nio/example1/Server.java
+++ goudong-modules/netty/src/main/java/com/goudong/modules/netty/nio/example1/Server.java
@@ -3,7 +3,10 @@ package com.goudong.modules.netty.nio.example1;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -17,44 +20,83 @@ import java.util.Set;
 public class Server {
     //~fields
     //==================================================================================================================
+    /**
+     * 端口号
+     */
+    public static final int PORT = 8080;
 
     //~methods
     //==================================================================================================================
     public static void main(String[] args) throws IOException {
+        // 创建ServerSocketChannel
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        // 创建选择器
         Selector selector = Selector.open();
+        // 设置非阻塞
         serverSocketChannel.configureBlocking(false);
-
+        // 绑定端口，和请求进入的最大连接队列
+        serverSocketChannel.socket().bind(new InetSocketAddress(PORT), 1024);
+        // 注册事件： 接受连接的事件
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-        serverSocketChannel.socket().bind(new InetSocketAddress(8080), 1024);
         System.out.println("服务启动成功");
+
         while(true) {
+            // 获取选择器中就绪的事件
             selector.select();
+            // 获取就绪事件的集合
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            // 创建迭代器
             Iterator<SelectionKey> iterator = selectionKeys.iterator();
             while (iterator.hasNext()) {
+                // 获取事件
                 SelectionKey selectionKey = iterator.next();
+                // 我们必须首先将处理过的 SelectionKey 从选定的键集合中删除。如果我们没有删除处理过的键，那么它仍然会在主集合中以一个激活的键出现，这会导致我们尝试再次处理它。
+                iterator.remove();
+                // 判断该键是否有效。一个键在创建时是有效的，并且一直保持有效，直到它被取消、它的通道被关闭或它的选择器被关闭。
+                if (selectionKey.isValid()) {
+                    // 处理新接入的客户端的请求
+                    if (selectionKey.isAcceptable()) {
+                        // 获取注册该事件的通道（上面的ServerSocketChannel）
+                        ServerSocketChannel ssc = (ServerSocketChannel) selectionKey.channel();
+                        // 接受连接，创建服务端SocketChannel
+                        SocketChannel socketChannel = ssc.accept();
+                        // 设置为非阻塞
+                        socketChannel.configureBlocking(false);
+                        // 创建服务端SocketChannel注册事件：读事件
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                        System.out.println("客户端连接成功");
+                    } else if (selectionKey.isReadable()) { // 处理读事件
+                        // 获取注册读事件的通道（ServerSocket）
+                        SocketChannel channel = (SocketChannel)selectionKey.channel();
+                        // 创建ByteBuffer，开辟一个缓冲区
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(128);
 
-                boolean valid = selectionKey.isValid();
-                if (!valid) {
-                    return;
-                }
+                        int read;
+                        try {
+                            // 从通道内读取数据，然后写入byteBuffer （客户端关闭连接，也会触发读事件，但是读取时会IOException）
+                            read = channel.read(byteBuffer);
+                        } catch (IOException e) {
+                            read = -1;
+                        }
 
-                if (selectionKey.isAcceptable()) {
-                    ServerSocketChannel ssc = (ServerSocketChannel) selectionKey.channel();
-                    SocketChannel socketChannel = ssc.accept();
-                    socketChannel.configureBlocking(false);
-                    socketChannel.register(selector, SelectionKey.OP_READ);
-                    System.out.println("客户端连接成功");
-                } else if (selectionKey.isReadable()) {
-                    SocketChannel channel = (SocketChannel)selectionKey.channel();
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(128);
-                    int read = channel.read(byteBuffer);
-                    if (read > 0) {
-                        System.out.println("接收到消息: " + new String(byteBuffer.array()));
-                    } else {
-                        channel.close();
+                        if (read > 0) {
+                            // 切换到读模式
+                            byteBuffer.flip();
+                            // 创建一个limit - position 大小的字节数组
+                            byte[] bytes = new byte[byteBuffer.remaining()];
+                            // 从byteBuffer中读取bytes大小的数据。
+                            byteBuffer.get(bytes);
+                            String msg = new String(bytes);
+                            System.out.println("接收到客户端消息: " + msg);
+                            // 给客户端做应答
+                            channel.write(ByteBuffer.wrap(("服务器收到消息:"+msg).getBytes("UTF-8")));
+                        } else if (read < 0) { //
+                            System.out.println("远程主机强迫关闭了一个现有的连接。");
+                            /*取消特定的注册关系*/
+                            selectionKey.cancel();
+                            /*关闭通道*/
+                            channel.close();
+                        }
                     }
                 }
             }
