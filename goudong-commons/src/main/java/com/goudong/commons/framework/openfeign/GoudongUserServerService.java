5859eab9d8a932d05b8bead8a001fac73890150d
陈飞龙 <1696741038@qq.com>
2023-06-18 20:43:51 +0800

    多租户重构

--- goudong-commons/src/main/java/com/goudong/commons/framework/openfeign/GoudongUserServerService.java
+++ goudong-commons/src/main/java/com/goudong/commons/framework/openfeign/GoudongUserServerService.java
@@ -1,28 +1,27 @@
-// package com.goudong.commons.framework.openfeign;
-//
-// import com.goudong.commons.dto.oauth2.BaseUserDTO;
-// import com.goudong.commons.dto.user.BaseUser2QueryPageReq;
-// import com.goudong.core.lang.PageResult;
-// import com.goudong.core.lang.Result;
-// import org.springframework.cloud.openfeign.FeignClient;
-// import org.springframework.cloud.openfeign.SpringQueryMap;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.ResponseBody;
-//
-// /**
-//  * User接口
-//  * @Author msi
-//  */
-// @FeignClient(name="goudong-user-server", path = "/api/user")
-// @ResponseBody
-// public interface GoudongUserServerService {
-//
-//
-//     /**
-//      * 分页查询
-//      * @param page
-//      * @return
-//      */
-//     @GetMapping("/base-user/page")
-//     Result<PageResult<BaseUserDTO>> pageUser (@SpringQueryMap BaseUser2QueryPageReq page);
-// }
+package com.goudong.commons.framework.openfeign;
+
+import com.goudong.commons.dto.user.BaseUser2QueryPageReq;
+import com.goudong.core.lang.PageResult;
+import com.goudong.core.lang.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * User接口
+ * @Author msi
+ */
+@FeignClient(name="goudong-user-server", path = "/api/user")
+@ResponseBody
+public interface GoudongUserServerService {
+
+
+    /**
+     * 分页查询
+     * @param page
+     * @return
+     */
+    @GetMapping("/base-user/page")
+    Result<PageResult> pageUser (@SpringQueryMap BaseUser2QueryPageReq page);
+}
--- goudong-commons/src/test/java/com/goudong/commons/dto/oauth2/UserContextTest.java
+++
@@ -1,7 +0,0 @@
-package com.goudong.commons.dto.oauth2;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserContextTest {
-
-}
