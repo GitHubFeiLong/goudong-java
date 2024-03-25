a59cdf654e331059da5f75a62fc2df6490e782bd
cfl <1696741038@qq.com>
2024-03-25 17:08:59 +0800

    删除认证服务

--- goudong-modules/authentication/server/src/main/java/com/goudong/authentication/server/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.goudong.authentication.server.service.dto;
--- goudong-modules/authentication/server/src/main/java/com/goudong/authentication/server/service/manager/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 因为用户角色权限都是多对多，所以避免交叉引用，使用manager进行处理业务
- */
-package com.goudong.authentication.server.service.manager;
